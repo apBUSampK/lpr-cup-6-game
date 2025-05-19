1d8edb51fe68d3594eee3f36b65ff7431f22f862
k-kashapov <vanyasik247@gmail.com>
2025-05-19 23:35:01 +0300

    Music added

--- scripts/base.gd
+++ scripts/base.gd
@@ -6,6 +6,8 @@ var locale_en := false
 var level_number := -1
 var game_inst : Node2D
 
+var audio_stream := AudioStreamMP3.new()
+
 enum DIALOGUES {
 	Initial = 1,
 	PracticeOnSimple = 2,
@@ -13,7 +15,18 @@ enum DIALOGUES {
 	AReallyDifficult = 4,
 }
 
+var MUSIC = {
+	"MENU": "res://music/Меню.mp3",
+	"INGAME": "res://music/InGame.mp3",
+	"DIALOG": "res://music/Dialogue.mp3"
+}
+
+func _set_music(path: String):
+	$Music.stream = audio_stream.load_from_file(path)
+	$Music.play()
+
 func _ready() -> void:
+	_set_music(MUSIC['MENU'])
 	if not FileAccess.file_exists("user://lprGame.save"):
 		print("No save data found")
 		$MenuControl/Menu.disable_continue()
@@ -39,6 +52,7 @@ func _on_menu_continue_game() -> void:
 		save_file.close()
 		print("loaded level number = ", level_number)
 	game_inst.show()
+	_set_music(MUSIC['INGAME'])
 	$MenuControl/Menu.hide()
 
 func _on_menu_new_game() -> void:
@@ -46,6 +60,7 @@ func _on_menu_new_game() -> void:
 	$MenuControl/Menu.hide()
 	$MenuControl/Dialogue.UpdateDialogue(DIALOGUES.Initial)
 	$MenuControl/Dialogue.show()
+	_set_music(MUSIC['DIALOG'])
 
 func _on_menu_options() -> void:
 	$MenuControl/Options.show()
@@ -67,17 +82,21 @@ func _on_options_music_lvl(level: float) -> void:
 func _on_options_sfx_lvl(level: float) -> void:
 	$SFX.volume_linear = level
 
+func _start_new_game_instance():
+	level_number = 1
+	game_inst = game_scene.instantiate()
+	add_child(game_inst)
+	game_inst.next.connect(_load_next_scene)
+	game_inst.pause.connect(_pause)
+	_set_music(MUSIC['INGAME'])
+
 func _on_dialogue_dialogue_end(dialogue_number: int) -> void:
 	match dialogue_number:
 		DIALOGUES.Initial:
 			$MenuControl/Dialogue.UpdateDialogue(DIALOGUES.PracticeOnSimple)
 		DIALOGUES.PracticeOnSimple:
 			$MenuControl/Dialogue.hide()
-			level_number = 1
-			game_inst = game_scene.instantiate()
-			add_child(game_inst)
-			game_inst.next.connect(_load_next_scene)
-			game_inst.pause.connect(_pause)
+			_start_new_game_instance()
 
 func _save_game() -> void:
 	var save_file = FileAccess.open("user://lprGame.save", FileAccess.WRITE)
@@ -86,6 +105,7 @@ func _save_game() -> void:
 
 func _pause() -> void:
 	_save_game()
+	_set_music(MUSIC['MENU'])
 	game_inst.hide()
 	$MenuControl/Menu.show()
 	$MenuControl/Menu.disable_new_game()
