f9c0ced93d18c0124e608b0f234fb5229d1e1696
Savva Savenkov <67229061+apBUSampK@users.noreply.github.com>
2025-05-20 18:21:44 +0300

    project settings and cat order

--- scripts/congrats.gd
+++ scripts/congrats.gd
@@ -2,5 +2,5 @@ extends Label
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and visible:
-		if event.is_action_pressed("ui_accept") and event.pressed and not event.is_echo():
+		if event.is_action_pressed("new_level") and event.pressed and not event.is_echo():
 			get_parent().emit_signal("next", get_parent().next_scene)
