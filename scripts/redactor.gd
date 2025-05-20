26e28bc9e22671a124ffacd1f3941801746f6d3a
apBUS_amp_K <sdsnm737@gmail.com>
2025-05-20 16:36:08 +0300

    fixes and tutorial re-run

--- scripts/redactor.gd
+++ scripts/redactor.gd
@@ -5,3 +5,11 @@ func _mouse_enter() -> void:
 
 func _mouse_exit() -> void:
 	PickupController.over_redactor = false
+
+func _process(delta: float) -> void:
+	if not PickupController.has_pickup:
+		var bodies := get_overlapping_bodies()
+		for body in bodies:
+			if body is Element:
+				body.emit_signal("pickable_removed")
+				body.queue_free()
